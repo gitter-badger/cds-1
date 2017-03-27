447956a46712358213f47500955a98978f6d848b
François SAMIN <francois.samin@gmail.com>
2017-03-27 21:09:32 +0200

    feat (api): leader election

--- engine/api/leaderelection/leaderelection.go
+++ engine/api/leaderelection/leaderelection.go
@@ -5,8 +5,11 @@ import (
 
 	"strings"
 
+	"fmt"
+
 	"github.com/ovh/cds/engine/api/database"
 	"github.com/ovh/cds/engine/log"
+	"github.com/ovh/cds/sdk"
 )
 
 var (
@@ -19,46 +22,64 @@ func Init(h, s string) {
 	hostname = h
 	cdsname = s
 
-	go registerRoutine()
-	go awolRoutine()
-	go electionRoutine()
+	heartbeatTicker := time.NewTicker(2 * time.Second).C
+	awolTicker := time.NewTicker(time.Second).C
+
+	for {
+		_db := database.DB()
+		if _db == nil {
+			log.Critical("Database unavailable")
+		}
+		db := database.DBMap(_db)
+		select {
+		case <-heartbeatTicker:
+			heartbeat()
+
+		case <-awolTicker:
+			b, err := IsLeader()
+			if err != nil {
+				log.Critical("IsLeader error : %s", err)
+			}
+			if b {
+				awol()
+			}
+		}
+	}
+
 }
 
 func IsLeader() (bool, error) {
 	return false, nil
 }
 
-func registerRoutine() {
-	for {
-		time.Sleep(1 * time.Second)
-		_db := database.DB()
-		if _db == nil {
-			continue
+func heartbeat() error {
+	time.Sleep(1 * time.Second)
+	_db := database.DB()
+	if _db == nil {
+		return fmt.Errorf("Database unavailable")
+	}
+	db := database.DBMap(_db)
+	res, err := db.Exec("update leader_election_host set heartbeat = $2 where id = $1", host.ID, time.Now())
+	if err != nil {
+		return sdk.WrapError(err, "leaderelection.registerRoutine> Unable to heartbeat %s", host.Name)
+	}
+	n, err := res.RowsAffected()
+	if err != nil {
+		return sdk.WrapError(err, "leaderelection.registerRoutine> Unable to heartbeat %s", host.Name)
+	}
+	//Insert if no rows has been updated
+	if n == 0 {
+		host = &CDSHost{
+			Hostname:  hostname,
+			Name:      cdsname,
+			IsLeader:  false,
+			Heartbeat: time.Now(),
 		}
-		db := database.DBMap(_db)
-		res, err := db.Exec("update leader_election_host set heartbeat = $2 where id = $1", host.ID, time.Now())
-		if err != nil {
-			log.Critical("leaderelection.registerRoutine> Unable to heartbeat %s : %s", host.Name, err)
-			continue
-		}
-		n, err := res.RowsAffected()
-		if err != nil {
-			log.Critical("leaderelection.registerRoutine> Unable to heartbeat %s : %s", host.Name, err)
-			continue
-		}
-		//Insert if no rows has been updated
-		if n == 0 {
-			host = &CDSHost{
-				Hostname:  hostname,
-				Name:      cdsname,
-				IsLeader:  false,
-				Heartbeat: time.Now(),
-			}
-			if err := db.Insert(host); err != nil {
-				log.Critical("leaderelection.registerRoutine> Unable to insert host %s : %s", host.Name, err)
-			}
+		if err := db.Insert(host); err != nil {
+			return sdk.WrapError(err, "leaderelection.registerRoutine> Unable to insert host %s", host.Name)
 		}
 	}
+	return nil
 }
 
 func electionRoutine() {
@@ -105,28 +126,21 @@ func electionRoutine() {
 	}
 }
 
-func awolRoutine() {
-	for {
-		time.Sleep(2 * time.Second)
-		_db := database.DB()
-		if _db == nil {
-			continue
-		}
-		db := database.DBMap(_db)
-		//Load all hosts
-		hosts, err := LoadAll(db)
-		if err != nil {
-			log.Warning("leaderelection.awolRoutine> Unable to load hosts : %s", err)
-			continue
-		}
-		for _, h := range hosts {
-			//If last Heartbeat is older than 10 seconds ago
-			if h.Heartbeat.Before(time.Now().Add(-10 * time.Second)) {
-				if _, err := db.Delete(&h); err != nil {
-					log.Warning("leaderelection.awolRoutine> Unable to delete hosts : %s %s", h.Name, err)
-				}
+func awol(db *gorp.DBMap) error {
+
+	db := database.DBMap(_db)
+	//Load all hosts
+	hosts, err := LoadAll(db)
+	if err != nil {
+		return sdk.WrapError(err, "leaderelection.awolRoutine> Unable to load hosts")
+	}
+	for _, h := range hosts {
+		//If last Heartbeat is older than 5 seconds ago
+		if h.Heartbeat.Before(time.Now().Add(-5 * time.Second)) {
+			if _, err := db.Delete(&h); err != nil {
+				return sdk.WrapError(err, "leaderelection.awolRoutine> Unable to delete hosts : %s")
 			}
 		}
 	}
-
+	return nil
 }
